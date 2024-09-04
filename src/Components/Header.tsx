9a0b6449bd0d25c3c7fc408920754deace0f4fd0
judithmf00 <judithmf00@gmail.com>
2024-09-04 16:26:13 +0200

    Responsive header acabado

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,8 @@
-
 export default function Header() {
   return (
-    <header className="header flex justify-between px-40 py-5">
-        <h1 className="header__title">Contador de calorías</h1>
-        <button className="header__button">Reiniciar App</button>
+    <header className="header flex justify-between items-center px-4 sm:px-10 md:px-20 lg:px-40 py-5">
+        <h1 className="header__title text-lg sm:text-xl md:text-2xl lg:text-3xl">Contador de calorías</h1>
+        <button className="header__button text-sm sm:text-base md:text-lg px-4 py-2">Reiniciar App</button>
     </header>
-  )
+  );
 }
